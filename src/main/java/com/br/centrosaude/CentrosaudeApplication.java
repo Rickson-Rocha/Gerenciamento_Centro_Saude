d97ef74b50ecf465c21e2aadaab6b8d4c66186f7
Roberto Rickson Câmara Rocha <20221148060011@ifrn.local>
2024-01-03 07:26:12 -0300

    first commit

---
+++ src/main/java/com/br/centrosaude/CentrosaudeApplication.java
@@ -0,0 +1,13 @@
+package com.br.centrosaude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentrosaudeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentrosaudeApplication.class, args);
+	}
+
+}
